322f476a5d64ba071fb62bce4c21d497be6d1b6c
spark <spark>
2023-12-10 05:21:01 +0000

    update

--- scripts/generate_xml.py
+++ scripts/generate_xml.py
@@ -10,23 +10,16 @@ import shutil, subprocess
 # Thread function to generate XML files
 def generate_xml_files():
     xml_count = 0    
-    xml_directory = '/tmp/transactions'
-    xml_directory_tmp = '/tmp/transactions_tmp'
+    local_xml_directory = '/tmp/transactions'
+    hdfs_xml_directory = '/in/transactions'
     generate_xml_interval = 10 #seconds
-    checkpoint = '/data/checkpoint'
 
-    if os.path.exists(xml_directory):
-        shutil.rmtree(xml_directory)
-    if os.path.exists(xml_directory_tmp):
-        shutil.rmtree(xml_directory_tmp)    
-    if os.path.exists(checkpoint):
-        shutil.rmtree(checkpoint)
-    os.mkdir(xml_directory)
-    os.mkdir(xml_directory_tmp)
-    os.mkdir(checkpoint)
+    if os.path.exists(local_xml_directory):
+        shutil.rmtree(local_xml_directory) 
+    os.mkdir(local_xml_directory)
 
+    subprocess.run(f'/opt/hadoop-3.3.6/bin/hdfs dfs -rm {hdfs_xml_directory}/*', shell=True)
 
-    subprocess.run('/opt/hadoop-3.3.6/bin/hdfs dfs -rm /transactions_xml/*', shell=True)
     while True:
         time.sleep(generate_xml_interval)
         root = ET.Element("Data")         
@@ -51,15 +44,14 @@ def generate_xml_files():
             result.text = random.choice(["Success", "Failure"])
 
         xml_data = minidom.parseString(ET.tostring(root, encoding='utf8', method='xml')).toprettyxml(indent="   ")
-        file_name = f"transaction_{xml_count}.xml"
-        tmp_file_path = f"{xml_directory_tmp}/{file_name}"    
-        file_path = f"{xml_directory}/{file_name}"
-        if not os.path.exists(xml_directory):
+        file_name = f"transaction_{int(time.time())}.xml"
+        local_file_path = f"{local_xml_directory}/{file_name}"
+        if not os.path.exists(local_xml_directory):
             # Create the folder
-            os.mkdir(xml_directory)
-        with open(tmp_file_path, "w") as file:
+            os.mkdir(local_xml_directory)
+        with open(local_file_path, "w") as file:
             file.write(xml_data)
-        subprocess.run(f'/opt/hadoop-3.3.6/bin/hdfs dfs -put {tmp_file_path} /transactions_xml', shell=True)
+        subprocess.run(f'/opt/hadoop-3.3.6/bin/hdfs dfs -put {local_file_path} {hdfs_xml_directory}/', shell=True)
         xml_count += 1
 
 generate_xml_files()
