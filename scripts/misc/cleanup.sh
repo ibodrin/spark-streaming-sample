8be6b7c75b4e3ab351017f1dddb257aac864ded2
spark <spark>
2023-12-11 22:50:30 +0000

    update

--- scripts/misc/cleanup.sh
+++ scripts/misc/cleanup.sh
@@ -2,5 +2,11 @@
 
 /opt/kafka_2.13-3.6.1/bin/kafka-topics.sh --delete --topic test-topic --bootstrap-server localhost:9092
 /opt/hadoop-3.3.6/bin/hdfs dfs -rm -r /checkpoint /raw /processed /in /dlq
-/opt/hadoop-3.3.6/bin/hdfs dfs -mkdir -p /checkpoint/raw /checkpoint/processed /raw /processed /in/transactions /dlq/processed /dlq/raw
+/opt/hadoop-3.3.6/bin/hdfs dfs -mkdir -p \
+    /checkpoint/raw/transactions \
+    /checkpoint/processed/transactions \
+    /raw/transactions /processed/transactions \
+    /in/transactions \
+    /dlq/processed/transactions \
+    /dlq/raw/transactions
 /opt/kafka_2.13-3.6.1/bin/kafka-topics.sh --create --topic test-topic --bootstrap-server localhost:9092
