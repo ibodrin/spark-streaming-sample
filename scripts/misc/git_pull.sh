6ad353786db0a3cc9cb6f4f06529549900844473
spark <spark>
2023-12-11 20:30:57 +0000

    update

---
+++ scripts/misc/git_pull.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+script_path=$(dirname $0)
+cd $script_path
+repo_path=$(git rev-parse --show-toplevel)
+cd $repo_path
+eval $(ssh-agent)
+ssh-add ~/.ssh/github_read
+git pull
--- scripts/misc/start_all.sh
+++ scripts/misc/start_all.sh
@@ -7,6 +7,8 @@ script_path=$(dirname $0)
 cd $script_path
 repo_path=$(git rev-parse --show-toplevel)
 
+rm $kafka_path/logs/*
+
 echo "Starting yarn"
 nohup $hdp_path/sbin/start-yarn.sh 2>&1 >/tmp/hadoop-yarn.log &
 seconds=60 ; echo "Sleeping ${seconds} seconds..." ; sleep ${seconds}
@@ -16,16 +18,20 @@ nohup $hdp_path/sbin/start-dfs.sh 2>&1 >/tmp/hadoop-dfs.log &
 
 echo "Starting kafka zookeper"
 nohup $kafka_path/bin/zookeeper-server-start.sh $kafka_path/config/zookeeper.properties 2>&1 >/tmp/kafka-zookeper.log &
-seconds=60 ; echo "Sleeping ${seconds} seconds..." ; sleep ${seconds}
+seconds=10 ; echo "Sleeping ${seconds} seconds..." ; sleep ${seconds}
 
 echo "Starting kafka server"
 nohup $kafka_path/bin/kafka-server-start.sh $kafka_path/config/server.properties 2>&1 >/tmp/kafka-server.log &
-seconds=60 ; echo "Sleeping ${seconds} seconds..." ; sleep ${seconds}
+seconds=10 ; echo "Sleeping ${seconds} seconds..." ; sleep ${seconds}
 
 echo "Cleaning up from previous runs"
 $repo_path/scripts/misc/cleanup.sh
 
-echo "Starting spark jobs"
+echo "Starting write_to_kafka"
 nohup $repo_path/scripts/shell/start_write_to_kafka.sh 2>&1 >/tmp/start_write_to_kafka.log &
+echo "Starting write_to_raw"
 nohup $repo_path/scripts/shell/start_write_to_raw.sh 2>&1 >/tmp/start_write_to_raw.log &
+echo "Starting generate_xml"
+nohup $repo_path/scripts/misc/start_generate_xml.sh 2>&1 >/tmp/start_write_to_raw.log &
+seconds=60 ; echo "Sleeping ${seconds} seconds..." ; sleep ${seconds}
 nohup $repo_path/scripts/shell/start_merge_to_processed.sh 2>&1 >/tmp/start_merge_to_processed.log &
