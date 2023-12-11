f4cf085e6aa185e6d3cccc75dc3dec93b5020570
spark <spark>
2023-12-11 20:32:28 +0000

    update

--- scripts/misc/git_push.sh
+++ scripts/misc/git_push.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+script_path=$(dirname $0)
+cd $script_path
+repo_path=$(git rev-parse --show-toplevel)
+cd $repo_path
+eval $(ssh-agent)
+ssh-add ~/.ssh/github_write
+
 git add --all
 git commit -m update
 git push
